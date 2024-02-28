aff862664cbf3d2619a0f87d84a93f5e8a2fc9db
mkpoli <git@mkpo.li>
2024-02-28 18:58:52 +0900

    Remove regex

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,9 @@
-use regex::Regex;
-
 pub fn is_katakana(c: char) -> bool {
-    let re = Regex::new(r"\p{Script_Extensions=Katakana}").unwrap();
-    re.is_match(c.to_string().as_str())
+    ('\u{30A1}'..='\u{31FF}').contains(&c)
 }
 
 pub fn is_cyrillic(c: char) -> bool {
-    let re = Regex::new(r"\p{Script_Extensions=Cyrillic}").unwrap();
-    re.is_match(c.to_string().as_str())
+    ('\u{0400}'..='\u{04FF}').contains(&c)
 }
 
 // pub fn is_hangul(c: char) -> bool {
